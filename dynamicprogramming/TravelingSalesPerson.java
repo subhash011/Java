6311b81f3bca4ba013894a1f59c847783f3da79c
subhash011 <subhash011011@gmail.com>
2020-03-14 20:56:36 +0530

    updates in dp

--- dynamicprogramming/TravelingSalesPerson.java
+++ dynamicprogramming/TravelingSalesPerson.java
@@ -1,5 +1,46 @@
+//https://www.geeksforgeeks.org/travelling-salesman-problem-set-1/
+//for code with bit masking
+//https://codingblocks.com/resources/travelling-salesman/
+
 package dynamicprogramming;
 
-public class TravelingSalesPerson {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+//recursive approach of order n!
+//base case : C({1,j},j) = dist(1,j) , C({1,1},1) = Integer.MAX_VALUE
+//C(S,i) = min over all j {C(S/{i},j) +  dist(i,j)}
 
+public class TravelingSalesPerson {
+    static int matrix[][];
+    static int n;
+    static int ALL_DONE;
+
+    public static int travelingSalesPerson(int mask, int pos) {
+        if (mask == ALL_DONE)
+            return matrix[pos][0];
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            if ((mask & (1 << i)) == 0) {
+                ans = Integer.min(travelingSalesPerson((mask | (1 << i)), i) + matrix[pos][i], ans);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(new File("input.txt")));
+        n = Integer.parseInt(br.readLine());
+        ALL_DONE = (1 << n) - 1;
+        matrix = new int[n][n];
+        for (int i = 0; i < matrix.length; i++) {
+            String str[] = br.readLine().split(" ");
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[i][j] = Integer.parseInt(str[j]);
+            }
+        }
+        int ans = travelingSalesPerson(1, 0);
+        System.out.println(ans);
+    }
 }
--- greedyalgorithms/HuffmanCoding.java
+++ greedyalgorithms/HuffmanCoding.java
@@ -3,23 +3,81 @@ package greedyalgorithms;
 import java.io.IOException;
 import java.nio.Buffer;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
+class HuffmanNode {
+    public Character value;
+    public Integer frequency;
+    public HuffmanNode lchild, rchild;
+
+    public HuffmanNode(char value, int frequency) {
+        this.frequency = frequency;
+        this.value = value;
+        this.lchild = null;
+        this.rchild = null;
+    }
+}
+
 public class HuffmanCoding {
+    public static void findCodes(HuffmanNode root, HashMap<Character, String> hmap, String str) {
+        if (root.lchild == null && root.rchild == null) {
+            if (root.value != ' ') {
+                hmap.put(root.value, str);
+                return;
+            }
+        }
+        findCodes(root.lchild, hmap, str + '0');
+        findCodes(root.rchild, hmap, str + '1');
+    }
+
+    public static HashMap<Character, String> huffmanCoding(char chars[], int freq[]) {
+        PriorityQueue<HuffmanNode> qu = new PriorityQueue<>((a, b) -> {
+            return a.frequency - b.frequency;
+        });
+        for (int i = 0; i < freq.length; i++) {
+            HuffmanNode nd = new HuffmanNode(chars[i], freq[i]);
+            qu.add(nd);
+        }
+        while (qu.size() != 1) {
+            HuffmanNode a = qu.poll();
+            HuffmanNode b;
+            if (qu.peek() != null) {
+                b = qu.poll();
+            } else {
+                b = null;
+            }
+            HuffmanNode node = new HuffmanNode(' ', a.frequency + b.frequency);
+            node.lchild = a;
+            node.rchild = b;
+            qu.add(node);
+        }
+        HuffmanNode root = qu.poll();
+        HashMap<Character, String> hmap = new HashMap<>();
+        int arr[] = new int[50];
+        findCodes(root, hmap, "");
+        return hmap;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(new File("input.txt")));
         String str1[] = br.readLine().split(" ");
         String str2[] = br.readLine().split(" ");
-        int chars[] = new int[str1.length];
+        char chars[] = new char[str1.length];
         int freq[] = new int[str1.length];
         for (int i = 0; i < chars.length; i++) {
-            chars[i] = Integer.parseInt(str1[i]);
-            freq[i] = Integer.parseInt(str1[i]);
+            chars[i] = str1[i].charAt(0);
+            freq[i] = Integer.parseInt(str2[i]);
         }
-
+        HashMap<Character, String> ans = huffmanCoding(chars, freq);
+        for (Character ch : ans.keySet()) {
+            System.out.println(ch + " : " + ans.get(ch));
+        }
     }
 }
